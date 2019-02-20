86b70cf351806051812e1d67ddefb5abf0ff596d
John Mann <jmann@consumer-edge.com>
2019-02-20 17:35:47 -0500

    updated for some testing

--- dash_app/plots.py
+++ dash_app/plots.py
@@ -7,7 +7,7 @@ from style import colors
 bar_plot =  {
                 'data': [
                     {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                    {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+                    {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': 'Montreal'},
                 ],
                 'layout': {
                     'title': "Bar Plot",
